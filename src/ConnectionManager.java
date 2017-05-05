28654077c8919edddf173072f608524918fbb380
Alan Lima <alima@outlook.com>
2017-05-05 13:45:32 +1000

    created connectionmanager class

---
+++ src/ConnectionManager.java
@@ -0,0 +1,55 @@
+import com.mysql.jdbc.Connection;
+//import com.mysql.jdbc.ResultSet;
+//import com.mysql.jdbc.Statement;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by alima on 5/5/17.
+ */
+public class ConnectionManager {
+    /**
+     * Declaring constants variables
+     * */
+    private static final String MySQL_DB = "LPA_eComms";
+    private static final String user      = "lpaecomms";
+    private static final String pass      = "lpaecomms";
+    private static final String url       = "jdbc:mysql://phpmyadmin.app/" + MySQL_DB;
+
+    private Connection conn;
+
+    public void connect(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection(url, user, pass);
+            //st = (Statement) conn.createStatement();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet executeQuery(String sql) throws Exception {
+        try{
+            Statement st = conn.createStatement();
+            st.closeOnCompletion();
+            return st.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new Exception("Error on execute the query.", e);
+        }
+    }
+
+    public void executeUpdate(String sql) throws Exception {
+        try{
+            Statement st = conn.createStatement();
+            st.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new Exception("Error on execute the query.", e);
+        }
+    }
+}
